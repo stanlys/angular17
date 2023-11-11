8973d0f66559b62bdd960ee7f0e8b30c8c0451e3
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-11-11 14:25:39 +0300

    feat: test new elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular17';
+  tests = [{
+    id: 1,
+    text: 'value 1'
+  },{
+    id:2,
+    text: 'value 2 '
+  }]
 }
