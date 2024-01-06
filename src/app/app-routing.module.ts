eb562f08daddfce49f767b0802a087d1ccb7748a
Stanlys <3995782+stanlys@users.noreply.github.com>
2024-01-06 15:31:38 +0300

    feat: add header component & angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '*', component: AppComponent },
+  { path: 'test', component: CalculatorComponent },
+  {
+    path: 'starts',
+    loadComponent: () =>
+      import('./components/cards/cards.component').then(
+        (m) => m.CardsComponent
+      ),
+  },
+  {
+    path: 'calc',
+    loadComponent: () =>
+      import('./components/calculator/calculator.component').then(
+        (m) => m.CalculatorComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardsComponent } from './components/cards/cards.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CardsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CardsComponent,
+    HeaderComponent,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [CardsComponent],
