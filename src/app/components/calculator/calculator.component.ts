e676f66d700113f4a184552233306862d63e521d
Stanlys <3995782+stanlys@users.noreply.github.com>
2024-01-06 20:09:02 +0300

    feat: calculate temp from spped

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss',
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  public form = new FormGroup({
+    value: new FormControl(0.0, Validators.required),
+    degree: new FormControl<number>(0),
+  });
+
+  public isError = false;
+
+  public isShowCalculatedValue = false;
+  public calculatedValue = 0;
+
+  public calc(): void {
+    if (!Number(this.form.controls['value'].value)) this.isError = true;
+    const degree = Number(this.form.controls['degree'].value) || 1;
+    const speed = Number(this.form.controls['value'].value);
+    this.calculatedValue = 60 / speed;
+    this.isShowCalculatedValue = true;
+  }
+}
