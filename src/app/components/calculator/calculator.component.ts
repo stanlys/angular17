c4bda560e83e95dd64f5087e9222306f30efc524
Stanlys <3995782+stanlys@users.noreply.github.com>
2024-01-07 09:57:34 +0300

    feat: correct calc

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -7,6 +7,9 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-calculator',
@@ -16,6 +19,9 @@ import {
     MatButtonModule,
     ReactiveFormsModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss',
@@ -23,19 +29,43 @@ import {
 export class CalculatorComponent {
   public form = new FormGroup({
     value: new FormControl(0.0, Validators.required),
-    degree: new FormControl<number>(0),
+    typeConvert: new FormControl<number>(1),
   });
 
   public isError = false;
 
   public isShowCalculatedValue = false;
-  public calculatedValue = 0;
+  public calculatedValue = '';
 
   public calc(): void {
-    if (!Number(this.form.controls['value'].value)) this.isError = true;
-    const degree = Number(this.form.controls['degree'].value) || 1;
-    const speed = Number(this.form.controls['value'].value);
-    this.calculatedValue = 60 / speed;
+    const typeConvert = Number(this.form.controls['typeConvert'].value) || 1;
+    this.isError = false;
+    if (!Number(this.form.controls['value'].value)) {
+      this.isError = true;
+      return;
+    }
+    let speed = Number(this.form.controls['value'].value);
+    if (speed < 0) speed = speed * -1;
+    this.calculatedValue =
+      typeConvert === 1 ? this.speedToTemp(speed) : this.tempToSpeed(speed);
     this.isShowCalculatedValue = true;
+  }
+
+  private speedToTemp(value: number): string {
+    const _calculatedValue = 60 / value;
+    const secPerKm = Math.floor(
+      (_calculatedValue - Math.floor(_calculatedValue)) * 60
+    );
+    const minPerKm = Math.floor(_calculatedValue);
+    return `Ваш темп ${minPerKm} мин ${secPerKm} сек`;
+  }
+
+  private tempToSpeed(value: number): string {
+    const _calculatedValue = 60 / value;
+    const secPerKm = Math.floor(
+      (_calculatedValue - Math.floor(_calculatedValue)) * 100
+    );
+    const minPerKm = Math.floor(_calculatedValue);
+    return `Ваша скорость ${minPerKm}.${secPerKm} км/ч`;
   }
 }
